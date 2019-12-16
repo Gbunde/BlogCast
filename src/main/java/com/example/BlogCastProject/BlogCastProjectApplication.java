22ba7f68ac2600561aada5867b195f6f0c2fd5eb
Gbunde <56592891+Gbunde@users.noreply.github.com>
2019-12-16 11:27:51 -0500

    first commit

---
+++ src/main/java/com/example/BlogCastProject/BlogCastProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.BlogCastProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCastProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogCastProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BlogCastProject/BlogCastProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BlogCastProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCastProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
